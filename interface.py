242739470e0589968e92b0cc1688b034ef4440ec
Anders Mannberg <anders.mannberg@softhouse.se>
2017-05-08 19:56:35 +0200

    Updated usage info

--- interface.py
+++ interface.py
@@ -19,7 +19,7 @@ def parse_input(args):
 
 def action_name_with_arguments(args):
     """Strip script name & arguments from arg string"""
-    
+
     try:
         del args[0]
         action = args.pop(0)
@@ -30,7 +30,16 @@ def action_name_with_arguments(args):
         raise InvalidInputException()
 
 def usage_info():
-    return """\
-    Usage: px [OPTIONS]
-        set [hours] [date] [project name]
-        """
+    indent = ' ' * 4
+    string = "\n"
+    string = string + "#### CliPX - Time tracking tool for the command line ####" + '\n\n'
+    string = string + indent + 'px set <hours> <date> <project_name>' + '\n'
+    string = string + indent + 'px show <hours> <(date | week_number)> [<project_name>]' + '\n'
+    string = string + indent + 'px addproj <project_name>' + '\n'
+    string = string + indent + 'px lsproj' + '\n'
+    string = string + '\n'
+    string = string + indent + '{:<10}'.format('set') + 'Set number of hours worked on a date for specific project' + '\n'
+    string = string + indent + '{:<10}'.format('show') + 'Show number of hours worked on a date or in a week, either for all or specific project(s)' + '\n'
+    string = string + indent + '{:<10}'.format('addproj') + 'Add new project' + '\n'
+    string = string + indent + '{:<10}'.format('lsproj') + 'List all saved projects' + '\n'
+    return string
